8592b8e3679168d95fc91e4e3a4b876bc0626ebc
NickSNFK <nwielgus@hotmail.com>
2023-01-13 14:51:59 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,11 @@
 import requests
 
+print('requests version')
 print(requests.__version__)
 
+print()
+print('Homepage of Google:')
 print(requests.get("http://www.google.com/").text)
+print()
+print('This script contains the following source code:')
+print(requests.get("https://raw.githubusercontent.com/NickSNFK/cmput404-lab1/main/script.py").text)
